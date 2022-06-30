2eaa8001c17810795b08015cb5a1a166af825c2f
fumtas1k <fumta01a7@gmail.com>
2022-07-01 08:50:44 +0900

    add:navbarの表示を動的に変化させる機能追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+/* navbarのlink先にいるときに、表示を変化させる */
+document.addEventListener("turbo:load", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const nowHref = document.location.href;
+  navLinks.forEach(navLink => {
+    if (navLink.href === nowHref) {
+      navLink.classList.add("active");
+    }
+  });
+});
