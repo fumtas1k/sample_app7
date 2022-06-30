1133a66cba53ca2d49479b8c25d3f01a475c799c
fumtas1k <fumta01a7@gmail.com>
2022-06-30 16:03:22 +0900

    fix:構文修正

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,11 +1,29 @@
 require "rails_helper"
 
 RSpec.describe "StaticPages", type: :system do
+  describe "root" do
+    context "rootにアクセスした場合" do
+      before do
+        visit root_path
+      end
+
+      it "homeページが表示される" do
+        expect(page).to have_content "Sample App"
+      end
+    end
+  end
+
   describe "home" do
     context "homeにアクセスした場合" do
-      it "homeページが表示される" do
+      before do
         visit static_pages_home_path
+      end
+
+      it "homeページが表示される" do
         expect(page).to have_content "Sample App"
+      end
+
+      it "設定したタイトルが表示される" do
         expect(page).to have_title "Home | Ruby on Rails Tutorial Sample App"
       end
     end
@@ -13,9 +31,15 @@ RSpec.describe "StaticPages", type: :system do
 
   describe "help" do
     context "helpにアクセスした場合" do
-      it "helpページが表示される" do
+      before do
         visit static_pages_help_path
+      end
+
+      it "helpページが表示される" do
         expect(page).to have_content "Help"
+      end
+
+      it "設定したタイトルが表示される" do
         expect(page).to have_title "Help | Ruby on Rails Tutorial Sample App"
       end
     end
@@ -23,9 +47,15 @@ RSpec.describe "StaticPages", type: :system do
 
   describe "about" do
     context "aboutにアクセスした場合" do
-      it "aboutページが表示される" do
+      before do
         visit static_pages_about_path
+      end
+
+      it "aboutページが表示される" do
         expect(page).to have_content "About"
+      end
+
+      it "設定したタイトルが表示される" do
         expect(page).to have_title "About | Ruby on Rails Tutorial Sample App"
       end
     end
